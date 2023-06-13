838e291ddce2c87f3ca9890d3c8924657deefa71
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-06-12 20:50:00 -0400

    completed labs

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+//Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
+document.addEventListener('DOMContentLoaded', () => {
+let p = document.getElementById('text');
+p.textContent = 'This is really cool!'
+})
